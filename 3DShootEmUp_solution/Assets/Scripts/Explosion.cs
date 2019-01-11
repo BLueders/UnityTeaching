01abde704604d920ea57c2fb368b084b4ba8a2bc
Benno Lüders <benno.lueders@googlemail.com>
2019-01-11 14:45:54 +0100

    3D version of shootemup exercise

---
+++ 3DShootEmUp_solution/Assets/Scripts/Explosion.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script handles the explosion animation on the explosion GameObject. 
+/// </summary>
+public class Explosion : MonoBehaviour
+{
+    public AudioClip explosionSound;
+
+    /// <summary>
+    /// Start is called by Unity. This will play our explosion sound 
+    /// </summary>
+    void Start()
+    {
+        if (explosionSound != null) {
+            AudioSource.PlayClipAtPoint(explosionSound, transform.position);
+        }
+        StartCoroutine(DestroyAfterSeconds(1));
+    }
+
+    /// <summary>
+    /// This is a coroutine waits "time" then destorys the gameobject.
+    /// </summary>
+    IEnumerator DestroyAfterSeconds(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
---
+++ 3DShootEmUp_solution/Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This script handles the spaceships functionality, including movement and shooting. The spaceship needs any Collider2D attached that has the trigger flag checked, so it can be hit
+/// by the asteroids and die.
+/// </summary>
+public class SpaceShip : MonoBehaviour {
+
+	[Tooltip("How fast is the spaceship moving left to right")]
+    public float speed = 5;
+	[Tooltip("How fast is the spaceship shooting")]
+    public float rateOfFire = 5;
+    [Tooltip("Prefab to be instantiated when shooting (Projectile)")]
+    public GameObject projectilePrefab;
+    [Tooltip("Prefab to be instantiated when dying (explosioin)")]
+    public GameObject explosionPrefab;
+    [Tooltip("An Audioclip that is played when the ship shoots")]
+    public AudioClip laserSound;
+
+    private float lastTimeFired = 0;
+
+    /// <summary>
+    /// This is called by Unity every frame. It handles the ships movement and checks if it should fire
+    /// </summary>
+    void Update() {
+
+        // move the ship left and right, depending on the horizontal input
+        transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+
+        // if the fire button is pressed and we waited long enough since the last shot was fired, FIRE!
+        if ((Input.GetButton("Fire1") || Input.GetButton("Fire2") || Input.GetButton("Fire3") || Input.GetButton("Jump")) && (lastTimeFired + 1 / rateOfFire) < Time.time) {
+            lastTimeFired = Time.time;
+            FireTheLasers();
+        } 
+    }
+
+    /// <summary>
+    /// This is called by Unity when the object collides with another object. It is only called, when both objects have any 2D collider attached, at least one of them is a trigger and at least of of
+    /// the two colliding GameObjects has a Rigidbody2D attached.
+    /// </summary>
+    void OnTriggerEnter(Collider other){
+
+        // if the other object has the asteroid tag, the destroy the ship and lose the game
+        if(other.CompareTag("Asteroid")){
+            Instantiate(explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+        }
+    }
+
+    void FireTheLasers(){
+        AudioSource.PlayClipAtPoint(laserSound, transform.position);
+        Instantiate(projectilePrefab, transform.position + Vector3.forward, Quaternion.identity);
+    }
+}
