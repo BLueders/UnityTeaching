dee22f52c35c03806da4135a1e846b6a067bcaf0
Benno Lüders <benno.lueders@googlemail.com>
2019-01-11 14:35:49 +0100

    Optimization Demo

---
+++ OptimizationDemo/Assets/ProfilerDemo/ProfilerTestScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Profiling;
+
+public class ProfilerTestScript : MonoBehaviour {
+
+    public int countTo = 1000000;
+
+    void Start() {
+	
+    }
+
+    void Update() {
+
+        Profiler.BeginSample("Profiler Test");
+        int i = 0;
+        while (i < countTo) {
+            i++;
+        }
+        Profiler.EndSample();
+
+    }
+}
