99771d7ee4e7578fed6938fb6921354156c9fe22
Benno Lueders <benno.lueders@gmail.com>
2020-02-11 10:56:18 +0100

    Updated several projects to new Unity version

---
+++ FlappyChicken_solution/Assets/Scripts/TesterScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TesterScript : MonoBehaviour
+{
+    public float speed;
+    public Sprite newSprite;
+
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        int test = 0;
+        test++;
+        test++;
+        test++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time >= 5 )
+        {
+            sr.sprite = newSprite;
+        }
+
+        float movement = speed * Time.deltaTime; // (1/fps)
+        //transform.position += new Vector3(movement, 0, 0);
+    }
+}
